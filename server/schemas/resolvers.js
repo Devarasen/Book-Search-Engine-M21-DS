25810123ed597d63a363840f54432341f607b40b
Devarasen <devarasen@gmail.com>
2023-10-28 07:58:56 +1100

    resolvers defined

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,65 @@
 const { User } = require("../models");
 const { AuthenticationError, signToken } = require("../utils/auth");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    // Add this query to fetch all users:
+    users: async () => {
+      return User.find({});
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      try {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      } catch (error) {
+        console.error("Error in addUser resolver:", error);
+        throw error;
+      }
+    },
+
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book } }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } }
+        );
+      }
+    },
+  },
+};
 
 module.exports = resolvers;
